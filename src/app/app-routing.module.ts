fac0647e806b2bdb9c4f2131b1f5db338ed0726c
IsraelMora <isramora3@gmail.com>
2022-12-09 11:52:22 -0400

    change routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,19 +2,25 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AngularFireAuthGuard, redirectUnauthorizedTo } from '@angular/fire/compat/auth-guard';
 
-const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['']);
-
 const routes: Routes = [
   {
-  path: '',
-  loadChildren: () => import('./modules/identity/identity.module').then(m => m.IdentityModule)
+    path: '',
+    loadChildren: () =>
+      import('./modules/identity/identity.module').then(
+        (m) => m.IdentityModule
+      ),
   },
   {
-  path: 'main',
-  loadChildren: () => import('./modules/main/main.module').then(m => m.MainModule),
-  // canActivate: [AngularFireAuthGuard],
-  data: { authGuardPipe: redirectUnauthorizedToLogin},
-  }
+    path: 'main',
+    loadChildren: () =>
+      import('./modules/main/main.module').then((m) => m.MainModule),
+    canActivate: [AngularFireAuthGuard],
+    data: { authGuardPipe: () => redirectUnauthorizedTo(['']) },
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
 ];
 
 @NgModule({
--- src/app/modules/main/main.routing.ts
+++ src/app/modules/main/main.routing.ts
@@ -2,19 +2,27 @@ import { MainComponent } from './main.component';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { 
+  {
     path: '',
     component: MainComponent,
     children: [
       {
         path: '',
-        loadChildren: () => import('../home/home.module').then(m => m.HomeModule)
+        loadChildren: () =>
+          import('../home/home.module').then((m) => m.HomeModule),
       },
       {
         path: 'perfilamiento',
-        loadChildren: () => import('../perfilamiento/perfilamiento.module').then(m => m.PerfilamientoModule)
+        loadChildren: () =>
+          import('../perfilamiento/perfilamiento.module').then(
+            (m) => m.PerfilamientoModule
+          ),
       },
-    ]
+      {
+        path: '**',
+        redirectTo: '',
+      },
+    ],
   },
 ];
 
